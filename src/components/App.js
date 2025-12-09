0563009389fededab3c766d4c89669faa15a0072
raushanraie01 <raushanraie01@gmail.com>
2025-12-09 15:58:02 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [flag, setFlag] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button onClick={() => setFlag(true)}>Click</button>
+      {flag && (
+        <p>
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
